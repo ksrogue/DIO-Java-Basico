8264f02728bedd7034093171a210c2ba7d55594d
David William <ksdavid90@gmail.com>
2023-10-05 12:27:43 -0300

    operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,25 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero++;
+
+        // System.out.println(numero);
+
+        int a,b;
+        a = 6;
+        b = 5;
+
+        String resultado = a != b ? "verdadeiro" : "falso";
+        // System.out.println(resultado);
+
+        boolean simNao = a != b;
+
+        // System.out.println(simNao);
+
+        if(a > b) {
+            System.out.println("Verdadeiro");
+        } else {
+            System.out.println("Falso");
+        }
+    }
+}
