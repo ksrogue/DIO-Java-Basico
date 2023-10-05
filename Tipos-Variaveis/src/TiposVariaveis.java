98905dca15ebfad1477d6e00b0d66678a15fea3f
David William <ksdavid90@gmail.com>
2023-10-05 11:16:28 -0300

    Tipos de variaveis

---
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        String meuNome = "David William dos Santos";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+
+        int numeroNormal = 25000;
+
+        double numeroReal = 3.56;
+
+        final double VALOR_PI = 3.14;
+
+    }
+}
